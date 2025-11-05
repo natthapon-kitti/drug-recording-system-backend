a4bcdd1393a3901efc1f760a9f0cf7c8e9d6bcca
Natthapon Kittiphakaprom <natthapon.kittiphakaprom@gmail.com>
2025-11-05 09:35:46 +0700

    Add cors to project

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mysql = require('mysql2')
+const cors = require('cors')
 const app = express()
 const port = 8000
 require('dotenv').config();
 
 app.use(express.json())
+app.use(cors())
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
